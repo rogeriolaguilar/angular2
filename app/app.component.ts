f694e5e13dff61fa27b800880f7f8354b6c39ff1
Rogério L Aguilar <rogerio.l.aguilar@gmail.com>
2016-07-14 17:42:17 -0300

    Refactoring: Separating header from car-parts in two components.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,56 +1,15 @@
 import { Component } from '@angular/core';
+import { CarPartsComponent} from './car-parts.component';
 
 @Component({
   selector: 'functions-pipes-app',
   template: `
   <h1>{{heading}}</h1>
-  <ul>
-    <li *ngFor="let race of races">
-      <h2>{{race.name}} {{race.entryFee | currency:'USD':true}}</h2>
-      <p>{{race.date | date:'MMM d, y, h:MM a'}}</p>
-      <p>{{race.about}}</p>
-      <button *ngIf="!race.isRacing">Enter Race</button>
-      <h3 *ngIf="race.isRacing">Already Racing</h3>
-    </li>
-  </ul>
-  <h2>Total cost: {{totalCost() | currency:'BRL':true}}</h2>
-  `
+  <car-parts></car-parts>`,
+  directives: [CarPartsComponent]
+
 })
 export class AppComponent {
-  heading = "Ultra Racing Schedule"
-  cash = 10000;
-  races = [{
-    "id": 1,
-    "name": "Daytona Thunderdome",
-    "date": new Date('2512-01-04T14:00:00'),
-    "about": "Race through the ruins of an ancient Florida battle arena.",
-    "entryFee": 3200,
-    "isRacing": false
-  }, {
-    "id": 2,
-    "name": "San Francisco Ruins",
-    "date": new Date('2512-07-03T20:00:00'),
-    "about": "Drift down the streets of a city almost sunk under the ocean.",
-    "entryFee": 4700,
-    "isRacing": true
-  }, {
-    "id": 3,
-    "name": "New York City Skyline",
-    "date": new Date('2512-07-12T21:00:00'),
-    "about": "Fly between buildings in the electronic sky.",
-    "entryFee": 4300,
-    "isRacing": true
-  }];
-  // Write your function here
-  totalCost(){
-    let cost=0;
-    for(let race of this.races){
-      if(race.isRacing){
-        cost+=race.entryFee;        
-      }
-    }
-    return cost;
-  }
-
+  heading = "Ultra Racing Schedule";
 }
 
